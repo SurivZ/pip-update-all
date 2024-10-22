c5ee436e4156adbf7a4421e8760c036b6bfdb0e9
David Serrano <franklinserrano23@gmail.com>
2024-10-22 14:55:30 -0500

    setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pip-update-all',
-    version='0.0.3',
+    version='0.0.2',
     author='SurivZ',
     author_email='franklinserrano23@email.com',
     description='Paquete para actualizar todos los paquetes instalados con pip',
